d9cbd67b7550e053fb81fbc02197136ce41f70b1
Kenji <re_kenji@hotmail.com>
2026-09-20 00:57:38 -0300

    Implementação da escala menor natural

--- src/notas_musicais/escalas.py
+++ src/notas_musicais/escalas.py
@@ -1,8 +1,9 @@
 NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
-ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
+ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11), 'menor': (0, 2, 3, 5, 7, 8, 10)}
 
 
 def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
+    # fmt: off
     """
     Gera uma escala a partir de uma tônica e uma tonalidade.
 
@@ -21,9 +22,10 @@ def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
         >>> escala('C', 'maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
 
-        >>> escala('a', 'maior')
-        {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+        >>> escala('a', 'menor')
+        {'notas': ['A', 'B', 'C', 'D', 'E', 'F', 'G'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
     """
+    # fmt: on
     tonica = tonica.upper()
     try:
         intervalos = ESCALAS[tonalidade]
